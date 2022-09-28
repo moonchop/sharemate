de686ef3f136198431294d6337059ffaed7a8d78
moonchop <minssu913@naver.com>
2022-09-28 16:28:43 +0900

    fix : folder name pages to activity

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,29 +1,28 @@
 import "./BottomNav.css";
 import { useFlow } from "../stackflow";
-
-type Navigate_Type = 'Main'|'Group'|'Community'|'MyPage';
+import { BiHome } from "react-icons/bi";
+type Navigate_Type = "Main" | "Group" | "Community" | "MyPage";
 
 const NAVIGATE_OBJ = [
-  {action: 'Main', content: '개인'},
-  {action:'Group',content:'그룹'},
-  {action:'Community',content:'커뮤니티'},
-  {action: 'MyPage',content:'내정보'},
+  { action: "Main", content: "개인" },
+  { action: "Group", content: "그룹" },
+  { action: "Community", content: "커뮤니티" },
+  { action: "MyPage", content: "내정보" },
 ];
-
 
 const BottomNav = () => {
   const { replace } = useFlow();
 
   return (
     <nav className="wrapper">
-      {
-        NAVIGATE_OBJ.map(({action,content})=>
-        (<div key={action} onClick={() => replace(action as Navigate_Type, {})}>{content}</div>))
-      }
+      {NAVIGATE_OBJ.map(({ action, content }) => (
+        <div key={action} onClick={() => replace(action as Navigate_Type, {})}>
+          {content}
+          <BiHome />
+        </div>
+      ))}
     </nav>
   );
 };
 
 export default BottomNav;
-
-
