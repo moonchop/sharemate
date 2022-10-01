2c488cb0ec4d0e17bc749d9144a573897aa26c27
moonchop <minssu913@naver.com>
2022-10-01 23:54:33 +0900

    fix : folder name pages to activity

--- src/stackflow.ts
+++ src/stackflow.ts
@@ -1,10 +1,10 @@
 import "@stackflow/basic-ui/index.css";
 import { stackflow } from "@stackflow/react";
 import { basicRendererPlugin } from "@stackflow/plugin-renderer-basic";
-import Main from "./pages/MainActivity";
-import Community from "./pages/CommunityActivity";
-import Group from "./pages/GroupActivity";
-import MyPage from "./pages/MyPageActivity";
+import Main from "./activity/MainActivity";
+import Community from "./activity/CommunityActivity";
+import Group from "./activity/GroupActivity";
+import MyPage from "./activity/MyPageActivity";
 
 const activities = {
   Main,
