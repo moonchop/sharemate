b9000c20ebe466fe4fe24e71a8ed161254752b3a
heeeeee0129 <s007kk@ajou.ac.kr>
2022-10-16 02:49:12 +0900

    feat: add useSaveFormData hook

---
+++ src/hooks/useSaveFormData.ts
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+export const useSaveFormData = (formName: string) => {
+  const setData = (data: any) =>
+    sessionStorage.setItem(formName, JSON.stringify(data));
+  useEffect(() => {
+    const data = sessionStorage.getItem(formName);
+    if (data === null) return;
+    Object.entries(JSON.parse(data)).map(([key, value]) => {
+      const element = document.getElementById(key);
+      if (element !== null) (element as any).value = value;
+    });
+  });
+
+  return { setData };
+};
