4706a58996ed5ca263ed93a9939b433b8e6a60c7
devnak <showri0108@naver.com>
2022-10-29 15:44:44 +0900

    refactor: GroupFedd CSS Unification

--- src/components/GroupFeed.tsx
+++ src/components/GroupFeed.tsx
@@ -27,8 +27,8 @@ const GroupFeed = () => {
       building: "일신관",
       room: 2,
       hashtag: ["청결민감", "소음민감", "아침형생활습관"],
-      text: "그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다.",
-      people: ["소프트웨어학과20세", "경영학과20세", "미디어학과 28세"],
+      text: "그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다.그룹소개글이빈다.그래보낭아",
+      people: ["20살 /", " 20살 /", " 28살", " (3명)"],
     },
     {
       id: 3,
@@ -36,7 +36,7 @@ const GroupFeed = () => {
       room: 2,
       hashtag: ["청결민감", "소음민감", "아침형생활습관"],
       text: "그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다.",
-      people: ["소프트웨어학과20세", "경영학과20세", "미디어학과 28세"],
+      people: ["20살 /", " 20살 /", " 28살", " (3명)"],
     },
     {
       id: 4,
@@ -44,7 +44,7 @@ const GroupFeed = () => {
       room: 2,
       hashtag: ["청결민감", "소음민감", "아침형생활습관"],
       text: "그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다.",
-      people: ["소프트웨어학과20세", "경영학과20세", "미디어학과 28세"],
+      people: ["20살 /", " 20살 /", " 28살", " (3명)"],
     },
     {
       id: 5,
@@ -52,7 +52,7 @@ const GroupFeed = () => {
       room: 2,
       hashtag: ["청결민감", "소음민감", "아침형생활습관"],
       text: "그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다.",
-      people: ["소프트웨어학과20세", "경영학과20세", "미디어학과 28세"],
+      people: ["20살 /", " 20살 /", " 28살", " (3명)"],
     },
     {
       id: 6,
@@ -60,7 +60,7 @@ const GroupFeed = () => {
       room: 4,
       hashtag: ["청결민감", "소음민감", "아침형생활습관"],
       text: "그룹소개글입니다.",
-      people: ["소프트웨어학과20세", "경영학과20세", "미디어학과 28세"],
+      people: ["20살 /", " 20살 /", " 28살", " (3명)"],
     },
     {
       id: 7,
@@ -68,7 +68,7 @@ const GroupFeed = () => {
       room: 4,
       hashtag: ["청결민감", "소음민감", "아침형생활습관"],
       text: "그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다. 그룹소개글입니다.",
-      people: ["소프트웨어학과20세", "경영학과20세", "미디어학과 28세"],
+      people: ["20살 /", " 20살 /", " 28살", " (3명)"],
     },
   ];
 
@@ -77,10 +77,10 @@ const GroupFeed = () => {
   return (
     <div className="h-[85%] overflow-y-scroll px-1">
       {groupData.map((elem: IGroup) => (
-        <div key={elem.id} className="h-auto">
-          <div className="flex my-2 justify-between">
+        <div key={elem.id} className="h-[20%] mt-2">
+          <div className="flex justify-between mb-2">
             <div className="flex flex-row ml-1 mr-2">
-              <div className="flex flex-col">
+              <div className="flex flex-col w-[90%] h-auto">
                 <p className="flex text-black mx-2">{`[${elem.building} ${elem.room}인실]`}</p>
                 <span className="flex space-x-3 px-3 py-2">
                   {/* <p className="flex items-center justify-center bg-white rounded-2xl ring-2  ring-[rgb(219,156,218)] text-[rgb(219,156,218)] text-sm px-3 py-1 ">{`#${elem.hashtag}`}</p> */}
@@ -90,7 +90,7 @@ const GroupFeed = () => {
                     color={HashTagColor as ("red" | "blue" | "green")[]}
                   />
                 </span>
-                <p className="flex ml-3 mr-3 text-xs mt-0.5 max-w-[76%]">
+                <p className="ml-3 mr-3 text-xs mt-0.5 w-[90%] h-[30px] overflow-hidden whitespace-pre-line	">
                   {elem.text}
                 </p>
                 <div className="flex space-x-4 mt-2 ml-4">
@@ -99,17 +99,17 @@ const GroupFeed = () => {
                   {/* {people.map((elem.person)=> (<p className="flex text-xs mt-0.5">{elem.person}</p>))} */}
                 </div>
               </div>
-              <div
-                className="flex text-center justify-center items-center px-2 py-0.5 rounded-lg text-xs font-medium bg-blue-100 text-blue-800"
-                onClick={() => {
-                  push("DetailGroupActivity", elem);
-                }}
-              >
-                attend
-              </div>
+            </div>
+            <div
+              className="flex text-center justify-center items-center px-2 py-0.5 mx-2 rounded-lg text-xs font-medium bg-blue-100 text-blue-800"
+              onClick={() => {
+                push("DetailGroupActivity", elem);
+              }}
+            >
+              attend
             </div>
           </div>
-          <hr className="mt-1" />
+          <hr className="" />
         </div>
       ))}
     </div>
