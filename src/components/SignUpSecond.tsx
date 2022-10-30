49b5fb79f397ebb36b24cb6ccb821d3e9293829a
heeeeee0129 <s007kk@ajou.ac.kr>
2022-10-30 13:55:27 +0900

    refactor: added tailwindCSS to useRadio hook

--- src/components/SignUpSecond.tsx
+++ src/components/SignUpSecond.tsx
@@ -1,19 +1,44 @@
 import { useRadio } from "../hooks/useRadio";
 
 const SignUpSecond = ({ handleGoPrev, handleGoNext }: any) => {
-  const { Component: GanderSelectComponent, onSubmit } = useRadio([
-    "1",
-    "2",
-    "3",
-    "4",
-    "기타",
-  ]);
-
+  const { Component: GenderSelectComponent } = useRadio(["남", "여"]);
+  const { Component: GradeSelectComponent } = useRadio(["1", "2", "3", "4"]);
+  const { Component: ForeignSelectComponent } = useRadio(["내국인", "외국인"]);
   return (
     <>
-      <GanderSelectComponent />
-      <button onClick={handleGoPrev}>이전</button>
-      <button onClick={handleGoNext}>다음</button>
+      <div className="my-10">
+        <p className="mt-2 ml-2">성별을 선택해주세요.</p>
+        <GenderSelectComponent />
+
+        <p className="mt-2 ml-2">출생년도를 입력해주세요.</p>
+        <div className="flex items-center justify-center mb-3">
+          <input
+            className="w-[70px] underline text-base text-coolGray-900 placeholder-slate-300"
+            id="code"
+            type="number"
+            name="code"
+            placeholder="2000"
+          />
+          <p>년도</p>
+        </div>
+
+        <p className="mt-2 ml-2">학년을 선택해주세요.</p>
+        <GradeSelectComponent />
+        <p className="mt-2 ml-2">외국인 여부</p>
+        <ForeignSelectComponent />
+      </div>
+      <button
+        onClick={handleGoPrev}
+        className="w-[100px] h-[44px] m-2 float-left  bg-[#FCABBE] hover:bg-[#F65B8082] font-medium text-sm text-black rounded-md shadow-button"
+      >
+        이전
+      </button>
+      <button
+        onClick={handleGoNext}
+        className=" w-[100px] h-[44px] m-2 float-right bg-[#FCABBE] hover:bg-[#F65B8082] font-medium text-sm text-black rounded-md shadow-button"
+      >
+        다음
+      </button>
     </>
   );
 };
--- src/hooks/useRadio.tsx
+++ src/hooks/useRadio.tsx
@@ -19,16 +19,30 @@ export const useRadio = (contentList: string[]) => {
   const RadioComponent = () => {
     return (
       <>
-        {contentList.map((content, index) => (
-          <div key={content}>
-            <p>{content}</p>
-            <input
-              type="radio"
-              onClick={() => handleClick(content)}
-              ref={(el) => (Ref.current[content] = el)}
-            />
+        <main className="flex w-full items-center justify-center p-2">
+          <div
+            className="grid w-[40rem] grid-flow-col-dense space-x-2 rounded-xl bg-gray-100 p-2"
+            x-data="app"
+          >
+            {contentList.map((content) => (
+              <div
+                key={content}
+                onClick={() => {
+                  handleClick(content);
+                }}
+              >
+                <input
+                  className="peer hidden"
+                  type="radio"
+                  ref={(el) => (Ref.current[content] = el)}
+                />
+                <label className=" block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-[#FCABBE] peer-checked:font-bold">
+                  {content}
+                </label>
+              </div>
+            ))}
           </div>
-        ))}
+        </main>
       </>
     );
   };
