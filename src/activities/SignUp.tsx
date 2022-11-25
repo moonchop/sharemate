f2586863d38d8fc6d931850d407bed967ce7dd9c
heeeeee0129 <s007kk@ajou.ac.kr>
2022-11-25 20:17:19 +0900

    fix: SignUp state number fix

--- src/activities/SignUp.tsx
+++ src/activities/SignUp.tsx
@@ -16,7 +16,7 @@ const Stage_Map = [
 ];
 
 const SignUp: ActivityComponentType = () => {
-  const [stage, setStage] = useState(2);
+  const [stage, setStage] = useState(0);
 
   const handleGoPrev = () => {
     console.log(stage);
