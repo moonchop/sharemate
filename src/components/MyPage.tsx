b9e2be732601ca538cc0ee30880bacf9a8735813
devnak <showri0108@naver.com>
2022-11-25 20:40:45 +0900

    feat: Logout, Delete Account & Mypage API

--- src/components/MyPage.tsx
+++ src/components/MyPage.tsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { IoIosArrowForward } from "react-icons/io";
 import { useFlow } from "../stackflow";
+import { useUser, useFavor } from "../stores/user";
+import { useAuth } from "../stores/auth";
+import request from "../stores/Request";
 
 const MyPage = () => {
   const { push, replace } = useFlow();
+  const { setToken } = useAuth();
+  const { name, email, profile_photo, userID, setUser } = useUser();
+  const { setFavor } = useFavor();
+
+  useEffect(() => {
+    request
+      .get("/user/mypage")
+      .then((response) => {
+        console.log(response.status);
+        console.log(response.data);
+        setUser(response.data.user);
+        setFavor(response.data.favor);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const logoutHandler = () => {
+    if (confirm("로그아웃 하시겠습니까?")) {
+      setToken({ accessToken: "", refreshToken: "" });
+      replace("LoginActivity", {});
+    } else {
+      alert("로그아웃이 취소되었습니다.");
+    }
+  };
+
+  const confirmHandler = () => {
+    if (confirm("회원탈퇴를 하시겠습니까?")) {
+      replace("LoginActivity", {});
+    } else {
+      alert("회원탈퇴가 취소되었습니다.");
+    }
+  };
+
   return (
     <div className="h-[85%] ">
       <div className="h-[32%] flex flex-col pro:pt-[30px] pt-[20px] border-b-[6px] border-[rgba(170,170,170,0.25)]">
@@ -15,12 +51,12 @@ const MyPage = () => {
             <div className="flex flex-row items-center w-[83%] ml-[24px]">
               <img
                 className="flex rounded-3xl pro:w-[80px] pro:h-[80px] w-[60px] h-[60px] mr-[15px]"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                src={profile_photo}
               />
               <div className="flex flex-col">
-                <div className="font-bold pro:text-xl text-lg">장은학님</div>
-                <div className="text-[#858181] pro:text-base text-sm">
-                  devnak
+                <div className="font-bold pro:text-xl text-lg">{name}</div>
+                <div className="text-[#858181] pro:text-sm text-xs">
+                  {email}
                 </div>
               </div>
             </div>
@@ -45,7 +81,7 @@ const MyPage = () => {
           <div className="pl-[28px] w-[90%] text-[#AFADF5] ">비밀번호 변경</div>
           <IoIosArrowForward
             className="opacity-30"
-            onClick={() => push("PasswordActivity", {})}
+            onClick={() => push("PasswordActivity", { userID })}
           ></IoIosArrowForward>
         </div>
       </div>
@@ -54,14 +90,14 @@ const MyPage = () => {
           <div className="pl-[28px] w-[90%] text-[#AFADF5] ">로그아웃</div>
           <IoIosArrowForward
             className="opacity-30"
-            onClick={() => replace("LoginActivity", {})}
+            onClick={logoutHandler}
           ></IoIosArrowForward>
         </div>
         <div className="h-[48%] flex items-center ">
           <div className="pl-[28px] w-[90%] text-[#AFADF5] ">회원 탈퇴</div>
           <IoIosArrowForward
             className="opacity-30"
-            onClick={() => replace("LoginActivity", {})}
+            onClick={confirmHandler}
           ></IoIosArrowForward>
         </div>
       </div>
