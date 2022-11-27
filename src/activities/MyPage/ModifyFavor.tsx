2ea5e5161653942ef9ee110d74401bfc705b76c8
heeeeee0129 <s007kk@ajou.ac.kr>
2022-11-27 15:51:42 +0900

    feat: Modify Favor use defaultValue

--- src/activities/MyPage/ModifyFavor.tsx
+++ src/activities/MyPage/ModifyFavor.tsx
@@ -1,16 +1,47 @@
+import { useState } from "react";
+import useModifyForm from "../../hooks/useModifyForm";
 import { useRegisterForm } from "../../hooks/useRegisterForm";
 import request from "../../stores/Request";
 
 const ModifyFavor = () => {
-  const { Component: ModifyForm, state } = useRegisterForm();
+  const { Component: ModifyForm, state } = useModifyForm();
+  const [intro, setIntro] = useState("");
 
   const submitHandler = () => {
-    request.put("/favor", {});
+    const result = { ...state, selfIntro: intro };
+    console.log(result);
+    request.put("/favor", { result });
   };
 
   return (
     <>
       <ModifyForm />
+      <div className="m-2 mb-14">
+        <p className="my-4 mt-10 ml-2 text-md">
+          자기소개 글을 입력해주세요.(50자 이내)
+        </p>
+        <input
+          className=" w-full h-[150px] px-4 mb-8 py-2.5 text-base text-coolGray-900 font-normal outline-none focus:border-[#F65B8082] border border-coolGray-200 rounded-lg shadow-input"
+          id="selfIntro"
+          type="text"
+          onChange={(e) => {
+            setIntro(e.target.value);
+            console.log(intro);
+          }}
+        />
+        <button
+          // onClick={handleGoPrev}
+          className="w-[100px] h-[44px] m-2 float-left ring-2 ring-[#ab82e0] text-[#ab82e0] font-extrabold text-sm borde rounded-md shadow-button"
+        >
+          이전
+        </button>
+        <button
+          onClick={submitHandler}
+          className="w-[100px] h-[44px] m-2 float-right ring-2 ring-[#ab82e0] text-[#ab82e0] font-extrabold text-sm borde rounded-md shadow-button"
+        >
+          수정하기
+        </button>
+      </div>
     </>
   );
 };
--- src/hooks/useModifyForm.tsx
+++ src/hooks/useModifyForm.tsx
@@ -1,91 +1,45 @@
+import { useMbti } from "./useRegisterForm";
 import { useEffect, useState } from "react";
 import { useRadio } from "./useRadio";
 
-export const useMbti = (defaultValue?: string[]) => {
-  const [mbti, setMbti] = useState<any>({
-    EI: null,
-    NS: null,
-    TF: null,
-    JP: null,
-  });
-
-  const { Component: EandISelect, state: EI } = useRadio({
-    contentList: ["E", "I"],
-    arrange: "row",
-    defaultState: defaultValue ? defaultValue[0] : mbti.EI,
-  });
-  const { Component: NandSSelect, state: NS } = useRadio({
-    contentList: ["N", "S"],
-    arrange: "row",
-    defaultState: defaultValue ? defaultValue[1] : mbti.NS,
-  });
-  const { Component: TandFSelect, state: TF } = useRadio({
-    contentList: ["T", "F"],
-    arrange: "row",
-    defaultState: defaultValue ? defaultValue[2] : mbti.TF,
-  });
-  const { Component: JandPSelect, state: JP } = useRadio({
-    contentList: ["J", "P"],
-    arrange: "row",
-    defaultState: defaultValue ? defaultValue[3] : mbti.JP,
-  });
-
-  useEffect(() => {
-    if (EI && NS && TF && JP)
-      setMbti({
-        EI,
-        NS,
-        TF,
-        JP,
-      });
-  }, [EI, NS, TF, JP]);
-
-  return {
-    state: mbti.EI + mbti.NS + mbti.TF + mbti.JP,
-    Component: () => (
-      <>
-        <p className="mt-2 ml-2 text-md">MBTI를 선택해주세요.</p>
-        <div className="flex items-center justify-center">
-          <EandISelect />
-          <NandSSelect />
-          <TandFSelect />
-          <JandPSelect />
-        </div>
-      </>
-    ),
-  };
-};
-
-export const useRegisterForm = () => {
-  const { Component: MbtiSelect, state: mbti } = useMbti();
+const useModifyForm = () => {
+  const { Component: MbtiSelect, state: mbti } = useMbti(["E", "N", "F", "P"]);
+  ////useMbti(string[]) <-- string 배열에 각 MBTI 값 넣어주면 됨
 
   const { Component: SleepTimeSelect, state: sleepTime } = useRadio({
     contentList: ["5시간 미만", "5시간 이상"],
     title: "수면 시간을 선택해주세요.",
+    defaultState: "5시간 미만", /// useRadio() <-- defaultState 에 string 값으로 기본 값 넣어주면 됨
   });
   const { Component: SmokingSelect, state: smoking } = useRadio({
     contentList: ["흡연", "비흡연"],
     title: "흡연 유무를 선택해주세요.",
+    defaultState: "흡연", //여기
   });
   const { Component: LivingPatternSelect, state: studyTime } = useRadio({
     contentList: ["올빼미형", "아침형"],
     title: "생활 패턴을 선택해주세요.",
+    defaultState: "올빼미형", //여기
   });
   const { Component: DrinkSelect, state: drinking } = useRadio({
     contentList: ["1", "2", "3", "4"],
     title: "일주일 내 음주 횟수를 입력해주세요.",
+    defaultState: "1", //여기
   });
   const { Component: CleannessSelect, state: cleanness } = useRadio({
     contentList: ["청결에 예민", "청결에 둔감"],
     title: "청결도를 선택해주세요.",
+    defaultState: "청결에 예민", //여기
   });
   const { Component: SnoringSelect, state: snoring } = useRadio({
     contentList: ["상", "중", "하", "무"],
     title: "코골이 유무를 선택해주세요.",
+    defaultState: "상", //여기
   });
   const { Component: WakeUpTime, state: wakeupTime } = useRadio({
     contentList: ["6-8시", "8-10시", "10-12시"],
     title: "기상 시간을 선택해주세요.",
+    defaultState: "6-8시", //여기
   });
 
   useEffect(() => {
@@ -113,7 +67,6 @@ export const useRegisterForm = () => {
   return {
     state: {
       mbti,
-
       sleepTime,
       smoking,
       studyTime,
@@ -140,3 +93,5 @@ export const useRegisterForm = () => {
     ),
   };
 };
+
+export default useModifyForm;
