397201322c5b0e37092588a5d697ee34c61e622e
hoonveloper <jh6car@gmail.com>
2022-11-28 18:50:52 +0900

    feat: 프로필사진

--- src/main/java/com/shareMate/shareMate/controller/auth/authController.java
+++ src/main/java/com/shareMate/shareMate/controller/auth/authController.java
@@ -6,6 +6,7 @@ import com.shareMate.shareMate.dto.sign.RequestSignInDto;
 import com.shareMate.shareMate.dto.sign.ResponseSignInDto;
 import com.shareMate.shareMate.entity.UserEntity;
 import com.shareMate.shareMate.exception.UserNotFoundException;
+import com.shareMate.shareMate.repository.UserRepository;
 import com.shareMate.shareMate.service.sign.SignService;
 import com.shareMate.shareMate.service.user.UserService;
 import io.swagger.annotations.ApiOperation;
@@ -16,11 +17,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 
@@ -31,6 +33,7 @@ import java.util.UUID;
 public class authController {
     private final UserService userService;
     private final SignService signService;
+    private final UserRepository userRepository;
 
 
 
@@ -86,5 +89,78 @@ public class authController {
 
         return ResponseEntity.status(HttpStatus.OK).body("SUCCESS");
     };
+
+
+
+    @PostMapping("/phototext")
+    public ResponseEntity<String> createFeed( @RequestParam("file") MultipartFile file,@RequestBody Map<String,String> json) {
+        System.out.println(json.get(json));
+        // 시간과 originalFilename으로 매핑 시켜서 src 주소를 만들어 낸다.
+        Date date = new Date();
+        StringBuilder sb = new StringBuilder();
+
+        // file image 가 없을 경우
+        if (file.isEmpty()) {
+            sb.append("none");
+        } else {
+            sb.append(date.getTime());
+            sb.append(file.getOriginalFilename());
+        }
+
+        if (!file.isEmpty()) {
+            File dest = new File("C://images/feed/" + sb.toString());
+            System.out.println("C://images/feed/" + sb.toString());
+            try {
+                file.transferTo(dest);
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return new ResponseEntity<String>("SUCCESS",HttpStatus.OK);
+    }
+    @PostMapping("/upload_ok")
+    public String upload(@RequestParam("file") MultipartFile file) {
+        String fileRealName = file.getOriginalFilename(); //파일명을 얻어낼 수 있는 메서드!
+        long size = file.getSize(); //파일 사이즈
+
+        System.out.println("파일명 : "  + fileRealName);
+        System.out.println("용량크기(byte) : " + size);
+        //서버에 저장할 파일이름 fileextension으로 .jsp이런식의  확장자 명을 구함
+        String fileExtension = fileRealName.substring(fileRealName.lastIndexOf("."),fileRealName.length());
+        //String uploadFolder = "/Users/kimjihoon/";
+        String uploadFolder = "src/main/resources/static";
+        File uploadPath= new File(uploadFolder,"upload");
+        if (!uploadPath.exists()) {uploadPath.mkdirs();
+            System.out.println("폴더만듬");}
+		    /*
+		  파일 업로드시 파일명이 동일한 파일이 이미 존재할 수도 있고 사용자가
+		  업로드 하는 파일명이 언어 이외의 언어로 되어있을 수 있습니다.
+		  타인어를 지원하지 않는 환경에서는 정산 동작이 되지 않습니다.(리눅스가 대표적인 예시)
+		  고유한 랜던 문자를 통해  db와 서버에 저장할 파일명을 새롭게 만들어 준다.
+		 */
+
+        UUID uuid = UUID.randomUUID();
+        System.out.println(uuid.toString());
+        String[] uuids = uuid.toString().split("-");
+
+        String uniqueName = uuids[0];
+        System.out.println("생성된 고유문자열" + uniqueName);
+        System.out.println("확장자명" + fileExtension);
+
+        // File saveFile = new File(uploadFolder+"\\"+fileRealName); uuid 적용 전
+
+        File saveFile = new File(uploadFolder+"/upload", uniqueName + fileExtension);  // 적용 후
+        try {
+            file.transferTo(saveFile); // 실제 파일 저장메서드(filewriter 작업을 손쉽게 한방에 처리해준다.)
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return uploadFolder+"upload/"+uniqueName+fileExtension;
+    }
 
 }
