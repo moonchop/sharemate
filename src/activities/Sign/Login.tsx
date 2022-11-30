e434d612fa5d907f4d564254b3c6e42b72bbfc59
devnak <showri0108@naver.com>
2022-12-01 00:08:16 +0900

    refactor: Modify Login Screen State

--- src/activities/Sign/Login.tsx
+++ src/activities/Sign/Login.tsx
@@ -49,100 +49,114 @@ const Login = () => {
     setLoginForm((prev) => ({ ...prev, pwd: "" }));
   };
 
+  useEffect(() => {
+    localStorage.getItem("auth-storage").length > 150
+      ? replace("MainActivity", {})
+      : "";
+  }, []);
+
   return (
-    <div className="h-full w-full">
-      <div className="flex flex-col items-center h-full w-full">
-        <div className="flex h-[50%] w-[63%] items-center justify-center mb-7">
-          <img src={Logo} className="flex mt-[54%] mb-[35%]" />
-        </div>
-        <div className="flex flex-col pro:h-[13%] h-[15%] w-[81%]">
-          <div className="flex w-full h-[50%] border-b-2 pro:mb-[7%] mb-[8%] items-center">
-            <input
-              className=" w-full h-full pb-1 outline-0 placeholder:text-base placeholder:font-normal border-none"
-              type={"email"}
-              placeholder="Email"
-              value={loginForm.email}
-              name="email"
-              onChange={changeHandler}
-            />
-            {loginForm.email ? (
-              <AiFillCloseCircle
-                className="w-[10%] h-[50%] opacity-60"
-                onClick={() => setLoginForm((prev) => ({ ...prev, email: "" }))}
-              />
-            ) : (
-              <></>
-            )}
-          </div>
-          <div className="flex w-full h-[50%] border-b-2 pro:mb-[7%] mb-[8%] items-center">
-            <input
-              className=" w-full h-full pb-1 outline-0 placeholder:text-base placeholder:font-normal border-none"
-              type={"password"}
-              placeholder="Password"
-              value={loginForm.pwd}
-              name="pwd"
-              onChange={changeHandler}
-            />
-            {loginForm.pwd ? (
-              <AiFillCloseCircle
-                className="w-[10%] h-[50%] opacity-60"
-                onClick={() => setLoginForm((prev) => ({ ...prev, pwd: "" }))}
-              />
-            ) : (
-              <></>
-            )}
-          </div>
-        </div>
-        <div className="w-[81%] justify-between items-center flex flex-row pro:mb-[15%] mb-[10%]">
-          <div
-            className="flex text-[#AAAAAA] text-sm mr-2 text  underline underline-offset-4"
-            onClick={() => {
-              replace("SignUpActivity", {});
-            }}
-          >
-            회원가입
-          </div>
-          <div
-            className="flex text-[#AAAAAA] text-sm  underline underline-offset-4"
-            onClick={() => {
-              push("FindPassword_EmailActivity", {});
-            }}
-          >
-            비밀번호 찾기
-          </div>
-        </div>
-        <button
-          className="w-[81%] h-[7%] rounded-md border border-transparent bg-[#B1A4FD] text-xl font-semibold text-white shadow-[0px_3px_5px_3px_rgba(0,0,0,0.25)] "
-          onClick={submitHandler}
-        >
-          Login
-        </button>
-        <div className="flex flex-col w-[60%] pro:mt-[50px] mt-[30px] text-xs text-[#AAAAAA] items-center">
-          <span>
-            <span>로그인을 누르는 것으로 </span>
-            <span
-              className="underline text-center text-slate-700"
-              onClick={() => push("ServiceTermActivity", {})}
+    <>
+      {localStorage.getItem("auth-storage").length < 150 && (
+        <div className="h-full w-full">
+          <div className="flex flex-col items-center h-full w-full">
+            <div className="flex h-[50%] w-[63%] items-center justify-center mb-7">
+              <img src={Logo} className="flex mt-[54%] mb-[35%]" />
+            </div>
+            <div className="flex flex-col pro:h-[13%] h-[15%] w-[81%]">
+              <div className="flex w-full h-[50%] border-b-2 pro:mb-[7%] mb-[8%] items-center">
+                <input
+                  className=" w-full h-full pb-1 outline-0 placeholder:text-base placeholder:font-normal border-none"
+                  type={"email"}
+                  placeholder="Email"
+                  value={loginForm.email}
+                  name="email"
+                  onChange={changeHandler}
+                />
+                {loginForm.email ? (
+                  <AiFillCloseCircle
+                    className="w-[10%] h-[50%] opacity-60"
+                    onClick={() =>
+                      setLoginForm((prev) => ({ ...prev, email: "" }))
+                    }
+                  />
+                ) : (
+                  <></>
+                )}
+              </div>
+              <div className="flex w-full h-[50%] border-b-2 pro:mb-[7%] mb-[8%] items-center">
+                <input
+                  className=" w-full h-full pb-1 outline-0 placeholder:text-base placeholder:font-normal border-none"
+                  type={"password"}
+                  placeholder="Password"
+                  value={loginForm.pwd}
+                  name="pwd"
+                  onChange={changeHandler}
+                />
+                {loginForm.pwd ? (
+                  <AiFillCloseCircle
+                    className="w-[10%] h-[50%] opacity-60"
+                    onClick={() =>
+                      setLoginForm((prev) => ({ ...prev, pwd: "" }))
+                    }
+                  />
+                ) : (
+                  <></>
+                )}
+              </div>
+            </div>
+            <div className="w-[81%] justify-between items-center flex flex-row pro:mb-[15%] mb-[10%]">
+              <div
+                className="flex text-[#AAAAAA] text-sm mr-2 text  underline underline-offset-4"
+                onClick={() => {
+                  replace("SignUpActivity", {});
+                }}
+              >
+                회원가입
+              </div>
+              <div
+                className="flex text-[#AAAAAA] text-sm  underline underline-offset-4"
+                onClick={() => {
+                  push("FindPassword_EmailActivity", {});
+                }}
+              >
+                비밀번호 찾기
+              </div>
+            </div>
+            <button
+              className="w-[81%] h-[7%] rounded-md border border-transparent bg-[#B1A4FD] text-xl font-semibold text-white shadow-[0px_3px_5px_3px_rgba(0,0,0,0.25)] "
+              onClick={submitHandler}
             >
-              서비스 이용약관
-            </span>
-            <span className="text-center">{"과\n"}</span>
-          </span>
-          <span>
-            <span
-              className=" underline text-center text-slate-700"
-              onClick={() => push("UserTermActivity", {})}
-            >
-              개인정보 처리 방침
-            </span>
-            <span className=" text-center">{"에 동의하고\n"}</span>
-          </span>
-          <span className="text-center">
-            서비스를 이용하는 것으로 간주합니다.
-          </span>
+              Login
+            </button>
+            <div className="flex flex-col w-[60%] pro:mt-[50px] mt-[30px] text-xs text-[#AAAAAA] items-center">
+              <span>
+                <span>로그인을 누르는 것으로 </span>
+                <span
+                  className="underline text-center text-slate-700"
+                  onClick={() => push("ServiceTermActivity", {})}
+                >
+                  서비스 이용약관
+                </span>
+                <span className="text-center">{"과\n"}</span>
+              </span>
+              <span>
+                <span
+                  className=" underline text-center text-slate-700"
+                  onClick={() => push("UserTermActivity", {})}
+                >
+                  개인정보 처리 방침
+                </span>
+                <span className=" text-center">{"에 동의하고\n"}</span>
+              </span>
+              <span className="text-center">
+                서비스를 이용하는 것으로 간주합니다.
+              </span>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
