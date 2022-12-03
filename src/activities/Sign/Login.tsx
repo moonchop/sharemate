c1051c4d282fb182e1baa76a511fd25cdb0d3531
devnak <showri0108@naver.com>
2022-12-03 19:12:15 +0900

    refactor: 로그인 페이지 APP에서 입력할 수 있게 수정

--- src/activities/Sign/Login.tsx
+++ src/activities/Sign/Login.tsx
@@ -60,13 +60,13 @@ const Login = () => {
       {localStorage.getItem("auth-storage").length < 150 && (
         <div className="h-full w-full">
           <div className="flex flex-col items-center h-full w-full">
-            <div className="flex h-[50%] w-[63%] items-center justify-center mb-7">
+            <div className="flex h-[30%] w-[63%] items-center justify-center mb-7">
               <img src={Logo} className="flex" />
             </div>
             <div className="flex flex-col h-28 w-[81%]">
-              <div className="flex w-full h-[50%] border-b-2 mb-6 items-center">
+              <div className="flex w-full h-[50%] border-b-[1px] border-black mb-6 items-center">
                 <input
-                  className=" w-full h-full pb-1 outline-0 placeholder:text-base placeholder:font-normal border-none"
+                  className=" w-full h-full pb-1 outline-0 placeholder:text-xl placeholder:font-normal border-none"
                   type={"email"}
                   placeholder="Email"
                   value={loginForm.email}
@@ -84,9 +84,9 @@ const Login = () => {
                   <></>
                 )}
               </div>
-              <div className="flex w-full h-[50%] border-b-2 mb-6 items-center">
+              <div className="flex w-full h-[50%] border-b-[1px] border-black mb-6 items-center">
                 <input
-                  className=" w-full h-full pb-1 outline-0 placeholder:text-base placeholder:font-normal border-none"
+                  className=" w-full h-full pb-1 outline-0 placeholder:text-xl placeholder:font-normal border-none"
                   type={"password"}
                   placeholder="Password"
                   value={loginForm.pwd}
@@ -123,35 +123,37 @@ const Login = () => {
                 비밀번호 찾기
               </div>
             </div>
-            <button
-              className="w-[81%] h-[7%] rounded-md border border-transparent bg-[#B1A4FD] text-xl font-semibold text-white shadow-[0px_3px_5px_3px_rgba(0,0,0,0.25)] "
-              onClick={submitHandler}
-            >
-              Login
-            </button>
-            <div className="flex flex-col w-[225px] mt-10 mb-3 text-xs text-[#AAAAAA] items-center">
-              <span>
-                <span>로그인을 누르는 것으로 </span>
-                <span
-                  className="underline text-center text-slate-700"
-                  onClick={() => push("ServiceTermActivity", {})}
-                >
-                  서비스 이용약관
+            <div className="absolute w-[81%] bottom-0">
+              <button
+                className="w-full h-14 rounded-md border border-transparent bg-[#B1A4FD] text-xl font-semibold text-white shadow-[0px_3px_5px_3px_rgba(0,0,0,0.25)] "
+                onClick={submitHandler}
+              >
+                Login
+              </button>
+              <div className="flex flex-col w-full mt-7 mb-3 text-xs text-center text-[#AAAAAA] items-center">
+                <span>
+                  <span>로그인을 누르는 것으로 </span>
+                  <span
+                    className="underline text-center text-slate-700"
+                    onClick={() => push("ServiceTermActivity", {})}
+                  >
+                    서비스 이용약관
+                  </span>
+                  <span className="text-center">{"과\n"}</span>
                 </span>
-                <span className="text-center">{"과\n"}</span>
-              </span>
-              <span>
-                <span
-                  className=" underline text-center text-slate-700"
-                  onClick={() => push("UserTermActivity", {})}
-                >
-                  개인정보 처리 방침
+                <span>
+                  <span
+                    className=" underline text-center text-slate-700"
+                    onClick={() => push("UserTermActivity", {})}
+                  >
+                    개인정보 처리 방침
+                  </span>
+                  <span className=" text-center">{"에 동의하고\n"}</span>
                 </span>
-                <span className=" text-center">{"에 동의하고\n"}</span>
-              </span>
-              <span className="text-center">
-                서비스를 이용하는 것으로 간주합니다.
-              </span>
+                <span className="text-center">
+                  서비스를 이용하는 것으로 간주합니다.
+                </span>
+              </div>
             </div>
           </div>
         </div>
