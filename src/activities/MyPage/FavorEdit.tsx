32d2c52b2d5784effadde2526edc0ba70d184c0b
devnak <showri0108@naver.com>
2022-12-14 13:23:59 +0900

    refactor: Modify FavorEdit

--- src/activities/MyPage/FavorEdit.tsx
+++ src/activities/MyPage/FavorEdit.tsx
@@ -3,18 +3,16 @@ import useModifyForm from "../../hooks/useModifyForm";
 import { useFlow } from "../../stackflow";
 import request from "../../stores/Request";
 import { useFavor, useUser } from "../../stores/user";
-import InputComponent from "../../components/InputComponent";
 
 const ModifyFavor = () => {
   const { pop } = useFlow();
   const { Component: ModifyForm, state } = useModifyForm();
   const [intro, setIntro] = useState("");
-  const [hash, setHash] = useState({ hashtags: ["", ""] });
   const { hashtags } = useUser();
-
+  const [hash, setHash] = useState<string[]>();
   const changeHashtag = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.currentTarget;
-    setHash((prev: any) => ({ ...prev, [hashtags[Number(name)]]: value }));
+    return setHash((prev: any) => ({ ...prev, [name]: value }));
   };
 
   const submitHandler = () => {
@@ -30,6 +28,7 @@ const ModifyFavor = () => {
       selfIntro: intro,
       hashtags: Object.values(hash),
     };
+    console.log(result);
     request
       .put("/favor", result)
       .then((response) => {
@@ -64,40 +63,60 @@ const ModifyFavor = () => {
     state.Cleanness = cleanness;
     state.Snoring = snoring;
     setIntro(selfIntro);
-    setHash({ hashtags: hashtags });
+    setHash(hashtags);
   }, []);
+
+  useEffect(() => {
+    hash &&
+      Object.entries(hash).map(([key, value]) => {
+        const element = document.getElementById(key);
+        if (element !== null) (element as any).value = value;
+      });
+  }, [hash, []]);
 
   return (
     <div className="px-3 pb-8 h-[93%] overflow-y-scroll scrollbar-hide">
       <ModifyForm />
       <div className="m-2 mb-14">
         <p className="text-base">자신을 표현하는 해시태그를 입력해주세요.</p>
-        <InputComponent
-          placeholder="성격좋음"
-          onChange={changeHashtag}
-          id="0"
-          //value={hash.hashtags[0]}
-        />
-        <InputComponent
-          placeholder="잘 안 들어옴"
-          onChange={changeHashtag}
-          id="1"
-          //value={hash.hashtags[1]}
-        />
-        <InputComponent
-          placeholder="조용함"
-          id="2"
-          onChange={changeHashtag}
-          //value={hash.hashtags[2]}
-        />
+
+        <div className="flex items-center justify-center rounded-xl bg-gray-100 py-3 mt-3">
+          <input
+            className="w-full text-center text-coolGray-900 bg-transparent placeholder-slate-300 placeholder:text-md outline-none"
+            id="0"
+            type="text"
+            name="0"
+            placeholder="성격좋음"
+            onChange={changeHashtag}
+          />
+        </div>
+        <div className="flex items-center justify-center rounded-xl bg-gray-100 py-3 mt-3">
+          <input
+            className="w-full text-center text-coolGray-900 bg-transparent placeholder-slate-300 placeholder:text-md outline-none"
+            id="1"
+            type="text"
+            name="1"
+            placeholder="잘 안 들어옴"
+            onChange={changeHashtag}
+          />
+        </div>
+        <div className="flex items-center justify-center rounded-xl bg-gray-100 py-3 mt-3">
+          <input
+            className="w-full text-center text-coolGray-900 bg-transparent placeholder-slate-300 placeholder:text-md outline-none"
+            id="2"
+            type="text"
+            name="2"
+            placeholder="조용함"
+            onChange={changeHashtag}
+          />
+        </div>
         <p className="my-4 mt-10 ml-2 text-md">
           자기소개 글을 입력해주세요.(50자 이내)
         </p>
-        <input
-          className=" w-full h-[150px] px-4 mb-8 py-2.5 text-base text-coolGray-900 font-normal outline-none focus:border-[#F65B8082] border border-coolGray-200 rounded-lg shadow-input"
+        <textarea
+          className="w-full h-[150px] px-4 mb-8 py-2.5 text-base text-coolGray-900 font-normal outline-none focus:border-[#F65B8082] border border-coolGray-200 rounded-lg shadow-input"
           id="selfIntro"
           value={intro}
-          type="text"
           onChange={(e) => {
             setIntro(e.target.value);
           }}
